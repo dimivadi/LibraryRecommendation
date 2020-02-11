d084d5c0bf8a2dc694538532e0522f9b2494fdbc
Dimitris Vadikolias <35259026+dimivadi@users.noreply.github.com>
2020-02-11 22:09:04 +0200

    Update Class1.java

--- Class1.java
+++ Class1.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -6,32 +7,71 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-public class Class1 {
-	
 
-	public static void main(String[] args) throws IOException{
+class Library{
+	String libraryName;
+	
+	Library(String libraryName){
+		this.libraryName = libraryName;
+	}
+}
+
+
+class Keyword{
+	String keywordName;
+	
+	Keyword(String keywordName){
+		this.keywordName = keywordName;
+	}
+	
+	String getKeyword() {
+		return keywordName;
+	}
+}
+
+class CodeFile{
+	
+	File codefile; 
+	
+	CodeFile(String filename){
+		this.codefile = new File(filename);
 		
-		ArrayList<String> libraries = new ArrayList<String>();
-		ArrayList<String> keywords = new ArrayList<String>();
-		
-		// 
-		ArrayList<String> stopwords = new ArrayList<String>();
-		
+	}
+	
+	File getFile() {
+		return codefile;
+	}
+	
+	
+	
+}
+
+class FindComponents{
+	
+	ArrayList<String> stopwords = new ArrayList<String>();
+	
+	FindComponents(String stopwordsFile) throws FileNotFoundException{
 		File swfile = new File("stopwords.txt");
 		Scanner sw = new Scanner(swfile);
 		while(sw.hasNext()) {
-			stopwords.add(sw.next());
+			this.stopwords.add(sw.next());
 		}
 		
 		sw.close();
-		
+	}
+	
+	void findLibraries(CodeFile codefile) {
 		//
+	}
+	
+	void findKeywords(CodeFile codeFile, ArrayList<Keyword> keywords) throws FileNotFoundException {
+		
+		
 		Pattern libPattern = Pattern.compile("(?<=(^\\s*import\\s))[\\w+\\.]+");
 		
 		Pattern wordPattern = Pattern.compile("[a-zA-Z]{3,}");
 		
-		File file = new File("ConnectedComponent.java");
-		Scanner in = new Scanner(file);
+		Scanner in = new Scanner(codeFile.getFile());
 		String line;
 		
 
@@ -42,24 +82,36 @@ public class Class1 {
 				String token = m.group();
 				if(!stopwords.contains(token))
 					if(!keywords.contains(token)) {
-						keywords.add(token);
+						Keyword keyword = new Keyword(token);
+						keywords.add(keyword);
 						}
 			}
 		}
-		for (String word : keywords)
-			System.out.println(word);
+		for (Keyword word : keywords)
+			System.out.println(word.getKeyword());
 		
 		
 		in.close();
 		
-
+		
+		
+		
+		
 	}
-
 }
 
 
-
-
-
-
+public class Class1{
+	
+	static ArrayList<Library> libraries = new ArrayList<Library>();
+	static ArrayList<Keyword> keywords = new ArrayList<Keyword>();
+	
+	public static void main(String[] args) throws IOException{
+		CodeFile codefile = new CodeFile("ConnectedComponent.java");
+		FindComponents find = new FindComponents("stopwords.txt");
+		find.findLibraries(codefile);//findLiraries static?
+		find.findKeywords(codefile, keywords);
+		
+	}
+}
 
