3cf0e8f1cf2962add4b6d87364359b25668c48be
Dimitris Vadikolias <35259026+dimivadi@users.noreply.github.com>
2020-02-29 19:11:00 +0200

    updated FilesList (list all files in folder)

--- FilesList.java
+++ FilesList.java
@@ -1,27 +1,51 @@
 package package1;
 
 import java.io.File;
-import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.List;
 
 class FilesList {
 	
-	private File[] files;
 	
-	FilesList(String folderPath){
-		
-		File dir = new File(folderPath);
-		files = dir.listFiles(new FilenameFilter()
-		{
-		  public boolean accept(File dir, String name)
-		  {
-		     return name.endsWith(".java");
-		  }
-		});
-		
-	
+	public static List<File> listAllFiles(String startDir, String extensions) {
+		String[] suffixes = toSuffixes(extensions);
+		System.out.println(suffixes);
+		return recurseDir(new File(startDir), suffixes);
 	}
 	
-	File[] getFiles() {
+	public static List<File> listAllFiles(File startDir, String extensions){
+		String[] suffixes = toSuffixes(extensions);
+		System.out.println(suffixes);
+		return recurseDir(startDir, suffixes);
+	}
+	
+	
+	public static List<File> recurseDir(File startDir, String[] suffixes) {
+		List<File> files = new ArrayList<File>();
+		
+		for(File item : startDir.listFiles()) {
+			if(item.isDirectory()) {
+				files.addAll(recurseDir(item, suffixes));
+			}else {
+				if(accept(item.getName(), suffixes))
+					files.add(item);
+			}
+		}
+		
 		return files;
 	}
+	
+	public static boolean accept(String fileName, String[] suffixes) {
+		for (String suffix : suffixes) {
+			if(fileName.endsWith(suffix))
+				return true;
+		}
+		return false;
+	}
+	
+	public static String[] toSuffixes(String extensions) {
+		return extensions.split("\\W+");
+	}
+	
+	
 }
