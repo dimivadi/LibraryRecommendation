e52d9bf215403157fa65f67a4d8f04f602a47694
Dimitris Vadikolias <35259026+dimivadi@users.noreply.github.com>
2020-03-22 22:27:03 +0200

    Add files via upload

---
+++ ComponentMiner.java
@@ -0,0 +1,9 @@
+package miners;
+
+import java.util.Collection;
+import datatypes.Component;
+
+public abstract class ComponentMiner {
+	
+	abstract Collection<Component> ComponentMining(Collection<Component> components);
+}
