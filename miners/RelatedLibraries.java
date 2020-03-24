a202b3811377b11c7f359c449bf9941e62889c1b
Dimitris Vadikolias <35259026+dimivadi@users.noreply.github.com>
2020-03-24 19:11:33 +0200

    Update RelatedLibraries.java

--- miners/RelatedLibraries.java
+++ miners/RelatedLibraries.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
+import datatypes.Library;
 
 import datatypes.Component;
 import datatypes.Connections;
