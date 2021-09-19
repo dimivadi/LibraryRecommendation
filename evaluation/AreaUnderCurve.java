e47554ab718fa01b499a3aec7553c3ea9c855cf5
Dimitris Vadikolias <35259026+dimivadi@users.noreply.github.com>
2021-09-19 14:28:10 +0300

    Add files via upload

--- evaluation/AreaUnderCurve.java
+++ evaluation/AreaUnderCurve.java
@@ -1,30 +1,63 @@
 package evaluation;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.apache.commons.math3.stat.inference.MannWhitneyUTest;
+
+import datatypes.Component;
 
 public class AreaUnderCurve extends Evaluate{
 
 	/*
 	 * 
 	 * Calculate the U statistic of Mann Whitney U Test, 
-	 * using as samples i)the libraries that the system should recommend in the testing set
-	 * and ii) the remaining libraries
+	 * using as samples i)the ranking of the libraries that the system should recommend in the testing set
+	 * and ii) the ranking of the remaining libraries
 	 * Then calculate the AUC according to the rule AUC = U/n1*n2
 	 * 
 	 */
 	
 	double[] x; //ranking of libraries that the system should recommend, according to testingSet
 	double[] y; //ranking of unwanted libraries
+	int i;
+	int j;
+	MannWhitneyUTest mw = new MannWhitneyUTest();
+//	List<Double> x = new ArrayList<Double>(); //ranking of libraries that the system should recommend, according to testingSet
+//	List<Double> y = new ArrayList<Double>(); //ranking of unwanted libraries
 	
-	AreaUnderCurve(String trainingSet, String testingSet, String filesExtensions) throws FileNotFoundException {
+	public AreaUnderCurve(String trainingSet, String testingSet, String filesExtensions) throws FileNotFoundException {
 		super(trainingSet, testingSet, filesExtensions);
 		
 	}
 
 	@Override
 	public void run() {
+		
+		Map<Component, Double> rankedComponents;
+		
+		for(Entry<Component, Set<Component>> entry: existingConnections.entrySet()) {
+			rankedComponents = componentMiner.componentMining(entry.getKey());
+			x = new double[entry.getValue().size()];
+			y = new double[rankedComponents.size()];
+			i = 0;
+			j = 0;
+			for(Map.Entry<Component, Double> rankedEntry: rankedComponents.entrySet()) {
+				if(entry.getValue().contains(rankedEntry.getKey())) {
+					x[i++] = rankedEntry.getValue();
+				}else {
+					y[j++] = rankedEntry.getValue();
+				}
+			}
+			
+			System.out.println("p value for keyword \""+entry.getKey()+"  "+  mw.mannWhitneyUTest(x, y));
+		}
+		
+		
 		
 		
 	}
