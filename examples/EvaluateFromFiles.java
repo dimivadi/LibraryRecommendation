ba51bd044177c824e2193519d7ef30a824dc40a5
Dimitris Vadikolias <35259026+dimivadi@users.noreply.github.com>
2021-10-19 21:11:28 +0300

    Add files via upload

---
+++ examples/EvaluateFromFiles.java
@@ -0,0 +1,113 @@
+package examples;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import datatypes.CodeFile;
+import datatypes.Component;
+import datatypes.Connections;
+import datatypes.FileNameToKeywords;
+import datatypes.FilesList;
+import datatypes.FindComponents;
+import datatypes.FindLibraries;
+import datatypes.FindLibrariesAndKeywords;
+import datatypes.Keyword;
+import datatypes.Library;
+import datatypes.Stopwords;
+import miners.ComponentMiner;
+import miners.RelatedLibraries;
+import evaluation.EvaluationDataProvider;
+
+
+/*
+ * 
+ * Class to provide essential fields for an evaluation method,
+ * i.e. the graph of components (ComponentMiner) 
+ * and the existing connections between components in the testing set (existingConnections)
+ * This class takes as input a string pointing to the training set, another one for the testing set, and a string indicating the extensions of the files to be used
+ * 
+ * trainingSet: path of the training set as string
+ * testingSet: path of the testing set as string
+ * filesExtensions: extensions of files to use, as string separated by non word characters
+ * 
+ */
+
+
+public class EvaluateFromFiles implements EvaluationDataProvider{
+	
+	List<File> trainingFiles; 
+	List<File> testingFiles;
+	Collection<Component> components;
+	FindComponents find = new FindLibrariesAndKeywords();
+	Connections connections = new Connections();
+	ComponentMiner componentMiner;
+	//Map that contains as keys a Set of seed components, and as values the libraries they are connected with
+	Map<Set<Component>, Set<Component>> existingConnections = new HashMap<>();
+	
+	/*
+
+	 */
+	public EvaluateFromFiles(String trainingSet, String testingSet, String filesExtensions) throws IOException{
+		
+		//update stopwords
+		Stopwords stopwords = new Stopwords();
+		stopwords.addStopwords(trainingSet);
+		stopwords.addStopwords(testingSet);
+		
+		
+		trainingFiles = FilesList.listAllFiles(trainingSet, filesExtensions);
+		testingFiles = FilesList.listAllFiles(testingSet, filesExtensions);
+		
+		List<CodeFile> trainingCodefiles = CodeFile.getCodeFiles(this.trainingFiles);
+		
+		
+		for(CodeFile codefile : trainingCodefiles){
+			components = find.findComponents(codefile);
+			connections.addConnectionsByType(components, Library.class, Keyword.class);
+		}
+		
+		componentMiner = new RelatedLibraries(connections);
+		
+		FileNameToKeywords fileNameToKeywords = new FileNameToKeywords();
+		
+		for(File file: testingFiles) {
+			FindComponents findLibraries = new FindLibraries();
+			Set<Component> fileLibraries = findLibraries.findComponents(new CodeFile(file));
+			
+			Set<Component> fileNameAsKeywords = fileNameToKeywords.getKeywords(file);
+			Set<String> stopwordsSet = stopwords.getStopwords();
+			
+			for (Iterator<Component> i = fileNameAsKeywords.iterator(); i.hasNext();) {
+			    Component element = i.next();
+			    if (stopwordsSet.contains(element.getName())) {
+			        i.remove();
+			    }
+			}
+			
+			if(existingConnections.putIfAbsent(fileNameAsKeywords, fileLibraries) == null)
+				continue;
+			else
+				existingConnections.get(fileNameAsKeywords).addAll(fileLibraries);
+			
+		}
+		
+
+	}
+	
+	public ComponentMiner getComponentMiner() {
+		
+		return componentMiner;
+	}
+	
+	public Map<Set<Component>, Set<Component>> getExistingConnections(){
+		
+		return existingConnections;
+	}
+	
+}
