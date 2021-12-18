afbedf93fec7e3166192fc230601666660edc474
Dimitris Vadikolias <35259026+dimivadi@users.noreply.github.com>
2021-12-18 15:31:45 +0200

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -17,15 +17,31 @@ public class MainClass{
 	
 	public static void main(String[] args) throws IOException{
 		
-
+		//get data
 //		EvaluationDataSource evaluationDataSource = new EvaluateFromFiles("jEdit", "test", "java");
 		EvaluationDataSource evaluationDataSource = new EvaluateFromMavenCentral("maven-data.csv/links_all.csv");
 		
-		
-		Connections connections = evaluationDataSource.getConnections();
-		ComponentMiner componentMiner = new RelatedLibraries(connections);
+		//get connections (part of the data) that will be used as a testing set
 		Map<Set<Component>, Set<Component>> existingConnections = evaluationDataSource.getExistingConnections();
 		
+		//get data structure to use as input for component miner
+		Connections connections = evaluationDataSource.getConnections();
+		
+		//instantiate a miner and create the graph
+		ComponentMiner componentMiner = new RelatedLibraries();
+		componentMiner.createGraph(connections);
+		
+		//call miner for a user defined seed vector
+		
+//		//get the components of the graph ranked, with respect to the seed vector 
+//		//and the class that implemented the ComponentMiner interface (e.g. keep only the library components of the graph)
+//		Map<Component, Double> rankedComponents = componentMiner.componentMining(seedComponents);
+//		//keep top-n of the ranked components
+//		RecommendedComponents rc = new RecommendedComponents(rankedComponents);
+//		rankedComponents = rc.getTopComponents(10);
+		
+		
+		//call an evaluation method 
 		
 		Evaluate evaluate = new AreaUnderCurve(componentMiner, existingConnections);
 //		Evaluate evaluate = new HitRate(componentMiner, existingConnections);
