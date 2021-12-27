1417c5da67cac054bef926c4ae749751d4547932
Dimitris Vadikolias <35259026+dimivadi@users.noreply.github.com>
2021-12-28 01:29:53 +0200

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,9 @@
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Map;
 import java.util.Set;
 
@@ -7,6 +11,7 @@ import datatypes.Component;
 import datatypes.Connections;
 import evaluation.*;
 import examples.*;
+import miners.ComponentGraph;
 import miners.ComponentMiner;
 import miners.RelatedLibraries;
 
@@ -17,19 +22,136 @@ public class MainClass{
 	
 	public static void main(String[] args) throws IOException{
 		
+		final boolean buildNewGraph = false;
+		final boolean b2 = true;
+		
+		String training = "training.ser";
+		String testing = "testing.set";
+		String graph = "graph.set";
+		
+		Map<Set<Component>, Set<Component>> existingConnections = null;
+		Connections connections = null;
+		ComponentMiner componentMiner;
+		ComponentGraph componentGraph = null;
+		
+		if(buildNewGraph) {
+		
+		
 		//get data
-//		EvaluationDataSource evaluationDataSource = new EvaluateFromFiles("jEdit", "test", "java");
-		EvaluationDataSource evaluationDataSource = new EvaluateFromMavenCentral("maven-data.csv/links_all.csv");
+//			EvaluationDataSource evaluationDataSource = new EvaluateFromFiles("jEdit", "test", "java");
+			EvaluationDataSource evaluationDataSource = new EvaluateFromMavenCentral("maven-data.csv/links_all.csv");
+			
+			//get connections (part of the data) that will be used as a testing set
+			existingConnections = evaluationDataSource.getExistingConnections();
+			
+			//get data structure to use as input for component miner
+			connections = evaluationDataSource.getConnections();
+			
+			
+			//Serialization
+			try {
+				
+				FileOutputStream file = new FileOutputStream(training);
+				ObjectOutputStream out = new ObjectOutputStream(file);
+				
+				out.writeObject(connections);
+				
+				out.close();
+				file.close();
+			
+				file = new FileOutputStream(testing);
+				out = new ObjectOutputStream(file);
+				
+				out.writeObject(existingConnections);
+				
+				out.close();
+				file.close();
+				
+				System.out.println("Objects have been serialized");
+			}catch(IOException ex)
+	        {
+	            System.out.println("IOException is caught");
+	        }
+			
+		}else {
 		
-		//get connections (part of the data) that will be used as a testing set
-		Map<Set<Component>, Set<Component>> existingConnections = evaluationDataSource.getExistingConnections();
+			//Deserialization
+			try {
+				FileInputStream file = new FileInputStream(training);
+				ObjectInputStream in = new ObjectInputStream(file);
+				
+				connections = (Connections) in.readObject();
+				
+				in.close();
+				file.close();
+				
+				file = new FileInputStream(testing);
+				in = new ObjectInputStream(file);
+				
+				existingConnections = (Map<Set<Component>, Set<Component>>) in.readObject();
+				
+				in.close();
+				file.close();
+				
+				System.out.println("Objects have been deserialized");
+				
+			}catch(IOException ex)
+	        {
+	            System.out.println("IOException is caught");
+	        }
+	          
+	        catch(ClassNotFoundException ex)
+	        {
+	            System.out.println("ClassNotFoundException is caught");
+	        }
 		
-		//get data structure to use as input for component miner
-		Connections connections = evaluationDataSource.getConnections();
+		}
 		
-		//instantiate a miner and create the graph
-		ComponentMiner componentMiner = new RelatedLibraries();
-		componentMiner.createGraph(connections);
+		if(b2) {
+			//instantiate a miner and create the graph
+			componentMiner = new RelatedLibraries();
+			componentMiner.createGraph(connections);
+			
+			try {
+				FileOutputStream file = new FileOutputStream(graph);
+				ObjectOutputStream out = new ObjectOutputStream(file);
+				
+				out.writeObject(componentMiner.getComponentGraph());
+				
+				out.close();
+				file.close();
+				
+				System.out.println("Graph has been serialized");
+				
+			}catch(IOException ex)
+	        {
+	            System.out.println("IOException is caught");
+	        }
+			
+		}else {
+			
+			try {
+				FileInputStream file = new FileInputStream(graph);
+				ObjectInputStream in = new ObjectInputStream(file);
+				
+				componentGraph = (ComponentGraph) in.readObject();
+				
+				in.close();
+				file.close();
+				
+				System.out.println("Graph has been deserialized");
+				
+			}catch(IOException ex)
+	        {
+	            System.out.println("IOException is caught");
+	        }
+	          
+	        catch(ClassNotFoundException ex)
+	        {
+	            System.out.println("ClassNotFoundException is caught");
+	        }
+		}
+		
 		
 		//call miner for a user defined seed vector
 		
@@ -43,8 +165,8 @@ public class MainClass{
 		
 		//call an evaluation method 
 		
-		Evaluate evaluate = new AreaUnderCurve(componentMiner, existingConnections);
-//		Evaluate evaluate = new HitRate(componentMiner, existingConnections);
+//		Evaluate evaluate = new AreaUnderCurve(componentMiner, existingConnections);
+		Evaluate evaluate = new HitRate(componentMiner, existingConnections);
 		
 		evaluate.run();
 	}
