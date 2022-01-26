15f25ebe7df69e1993fdcdeabc0a447f8acc8755
Dimitris Vadikolias <35259026+dimivadi@users.noreply.github.com>
2022-01-26 17:52:23 +0200

    Add files via upload

--- evaluation/Metrics.java
+++ evaluation/Metrics.java
@@ -1,29 +1,32 @@
 package evaluation;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.Map.Entry;
 
 import org.apache.commons.math3.stat.inference.MannWhitneyUTest;
 
 import datatypes.Component;
+import datatypes.Library;
 import miners.ComponentMiner;
 import miners.RankedComponents;
 
-public class Metrics implements Evaluate{
+public class Metrics{
 	
-	ComponentMiner componentMiner;
-	Map<Set<Component>, Set<Component>> existingConnections;
-	int numOfRecommendedComponents;
+	private ComponentMiner componentMiner;
+	private Map<Set<Component>, Set<Component>> existingConnections;
 	
 	
-	public Metrics(ComponentMiner componentMiner, Map<Set<Component>, Set<Component>> existingConnections, int numOfRecommendedComponents){
+	public Metrics(ComponentMiner componentMiner, Map<Set<Component>, Set<Component>> existingConnections){
 		this.componentMiner = componentMiner;
 		this.existingConnections = existingConnections;
-		this.numOfRecommendedComponents = numOfRecommendedComponents;
 	}
 	
 	public void run() {
+		
+		
 		double[] x; //ranking of libraries that the system should recommend, according to testingSet
 		double[] y; //ranking of unwanted libraries
 		int i;
@@ -32,13 +35,16 @@ public class Metrics implements Evaluate{
 		int N = 0;
 		double recall = 0;
 		double precision = 0;
+		double meanPrecision;
+		double meanRecall;
+		double f1Score;
 		int hits = 0;
 		double sumOfAUC = 0;
 		int sizeOfTestingSet = existingConnections.size();
 		
 		// 	Coverage
 		Coverage coverage = new Coverage();
-		coverage.setGraph(componentMiner.getComponentGraph());
+		coverage.setNumberOfCommonLibraries(componentMiner, existingConnections);
 		
 		for(Entry<Set<Component>, Set<Component>> existingConnection : existingConnections.entrySet()) {
 			
@@ -54,8 +60,8 @@ public class Metrics implements Evaluate{
 			y = new double[rankedComponents.size()];
 			i = 0;
 			j = 0;
-			System.out.println("Key: "+ existingConnection.getKey());
-			System.out.println("value: "+ existingConnection.getValue());
+
+			System.out.println("value: ("+existingConnection.getValue().size()+") "+ existingConnection.getValue());
 			for(Map.Entry<Component, Double> rankedEntry: rankedComponents.entrySet()) {
 				if(existingConnection.getValue().contains(rankedEntry.getKey())) {
 					x[i++] = rankedEntry.getValue();
@@ -68,53 +74,57 @@ public class Metrics implements Evaluate{
 			double auc = U/(x.length * y.length);
 			sumOfAUC += auc;
 			N++;
-//			System.out.println("AUC for keyword \""+existingConnection.getKey()+"\":  "+ auc);
-			System.out.println("AUC for seed "+existingConnection.getKey()+ ": " +auc);
+
+			System.out.println("AUC: "+ auc);
 		
 			RankedComponents rc = new RankedComponents(rankedComponents);
-//			Map<Component, Double> topComponents = rc.getTopComponents(numOfRecommendedComponents);
-			Map<Component, Double> topComponentsH = rc.getTopComponents(3);
-			Map<Component, Double> topComponentsR = rc.getTopComponents(5);
-			Map<Component, Double> topComponentsP = rc.getTopComponents(10);
-			
-			
-			coverage.addToRecommendedComponents(topComponentsR);
+			Map<Component, Double> top3Components = rc.getTopComponents(3);
+			Map<Component, Double> top5Components = rc.getTopComponents(5);
+			Map<Component, Double> top10Components = rc.getTopComponents(10);
+
+			coverage.addToRecommendedComponents(top5Components);
 			
 			//Recall
 			int count = 0;
 			
 			for(Component existingLibrary: existingConnection.getValue()) {
-				if(topComponentsR.containsKey(existingLibrary)) {
+				if(top10Components.containsKey(existingLibrary)) {
 					count++;
+				}else {
+					rc.getLibraryPosition(existingLibrary);
 				}
 			}
 			recall += (double) count / existingConnection.getValue().size();
-			System.out.println("recall for "+existingConnection.getKey()+" for top "+topComponentsR.size()+" components: "+(double) count / existingConnection.getValue().size());
+			System.out.println("recall@"+top10Components.size()+": "+ (double) count / existingConnection.getValue().size());
 			
 			//Precision
 			count = 0;
-			for(Map.Entry<Component, Double> recommendedComp : topComponentsP.entrySet()) {
+			for(Map.Entry<Component, Double> recommendedComp : top10Components.entrySet()) {
 				if(existingConnection.getValue().contains(recommendedComp.getKey())) {
 					count++;
 				}
 			}
-			precision += (double) count / (topComponentsP.size());
-
-			System.out.println("precision for "+existingConnection.getKey()+" for top "+topComponentsP.size()+" components: "+(double) count / topComponentsP.size());
+			precision += (double) count / (top10Components.size());
+			System.out.println("precision@"+top10Components.size()+": " + (double) count / top10Components.size());
 			
-			
+			f1Score = 2 / ((1/precision) + (1/recall));
 			// HitRate
-			for(Map.Entry<Component, Double> recommendedComp : topComponentsH.entrySet()) {
-				//System.out.println("recommendedComp: "+recommendedComp.getKey()+" for keyword: "+ entry.getKey());
+			for(Map.Entry<Component, Double> recommendedComp : top3Components.entrySet()) {
+
 				if(existingConnection.getValue().contains(recommendedComp.getKey())) {
 					hits++;
 					break;
 				}
 			}
 		}
+		meanRecall = (double) recall/sizeOfTestingSet;
+		meanPrecision = (double) precision/sizeOfTestingSet;
+		f1Score = (double) 2 / ((1/meanPrecision) + (1/meanRecall));
+		
 		System.out.println("AUC mean: "+ sumOfAUC/N);
-		System.out.println("mean recall: " + (double) recall/sizeOfTestingSet);
-		System.out.println("mean pricision: " + (double) precision/sizeOfTestingSet);
+		System.out.println("mean recall: " + meanRecall);
+		System.out.println("mean pricision: " + meanPrecision);
+		System.out.println("f1 score: " + f1Score);
 		System.out.println("Hit Rate: " + (float) hits/sizeOfTestingSet);
 		System.out.println("Coverage: " + coverage.getCoverage());
 	}
