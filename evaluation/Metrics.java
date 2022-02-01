ef0ef2f8f40d28f9e2498686f1e1e51abc84dda2
Dimitris Vadikolias <35259026+dimivadi@users.noreply.github.com>
2022-02-01 17:26:36 +0200

    Add files via upload

--- evaluation/Metrics.java
+++ evaluation/Metrics.java
@@ -1,15 +1,14 @@
 package evaluation;
 
-import java.util.HashSet;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.Map.Entry;
 
 import org.apache.commons.math3.stat.inference.MannWhitneyUTest;
 
 import datatypes.Component;
-import datatypes.Library;
 import miners.ComponentMiner;
 import miners.RankedComponents;
 
@@ -17,12 +16,26 @@ public class Metrics{
 	
 	private ComponentMiner componentMiner;
 	private Map<Set<Component>, Set<Component>> existingConnections;
+	private boolean sweepRatio;
+	private double dampingFactor;
+	private String normalization;
+	private FileWriter fileWriter;
 	
-	
-	public Metrics(ComponentMiner componentMiner, Map<Set<Component>, Set<Component>> existingConnections){
+	public Metrics(ComponentMiner componentMiner, Map<Set<Component>, Set<Component>> existingConnections, 
+			boolean sweepRatio, double dampingFactor, String normalization, FileWriter fileWriter){
 		this.componentMiner = componentMiner;
 		this.existingConnections = existingConnections;
+		this.sweepRatio = sweepRatio;
+		this.dampingFactor = dampingFactor;
+		this.normalization = normalization;
+		this.fileWriter = fileWriter;
 	}
+	
+//	public Metrics(ComponentMiner componentMiner, Map<Set<Component>, Set<Component>> existingConnections){
+//		this.componentMiner = componentMiner;
+//		this.existingConnections = existingConnections;
+//
+//	}
 	
 	public void run() {
 		
@@ -42,6 +55,8 @@ public class Metrics{
 		double sumOfAUC = 0;
 		int sizeOfTestingSet = existingConnections.size();
 		
+		System.out.println("size of testing set: "+sizeOfTestingSet);
+		
 		Similarity similarity = new Similarity(componentMiner);
 		
 		// 	Coverage
@@ -54,7 +69,8 @@ public class Metrics{
 				sizeOfTestingSet--;
 				continue;
 			}
-			Map<Component, Double> rankedComponents = componentMiner.componentMining(existingConnection.getKey());
+			Map<Component, Double> rankedComponents = componentMiner.componentMining(existingConnection.getKey(), sweepRatio, dampingFactor, normalization);
+//			Map<Component, Double> rankedComponents = componentMiner.componentMining(existingConnection.getKey());
 //			Map<Component, Double> rankedComponents = similarity.getLibrarySimilarity(existingConnection.getKey());
 			
 			// AUC
@@ -63,7 +79,7 @@ public class Metrics{
 			i = 0;
 			j = 0;
 
-			System.out.println("value: ("+existingConnection.getValue().size()+") "+ existingConnection.getValue());
+//			System.out.println("value: ("+existingConnection.getValue().size()+") "+ existingConnection.getValue());	//
 			for(Map.Entry<Component, Double> rankedEntry: rankedComponents.entrySet()) {
 				if(existingConnection.getValue().contains(rankedEntry.getKey())) {
 					x[i++] = rankedEntry.getValue();
@@ -77,7 +93,7 @@ public class Metrics{
 			sumOfAUC += auc;
 			N++;
 
-			System.out.println("AUC: "+ auc);
+//			System.out.println("AUC: "+ auc);
 		
 			RankedComponents rc = new RankedComponents(rankedComponents);
 			Map<Component, Double> top3Components = rc.getTopComponents(3);
@@ -97,7 +113,7 @@ public class Metrics{
 				}
 			}
 			recall += (double) count / existingConnection.getValue().size();
-			System.out.println("recall@"+top10Components.size()+": "+ (double) count / existingConnection.getValue().size());
+//			System.out.println("recall@"+top10Components.size()+": "+ (double) count / existingConnection.getValue().size());
 			
 			//Precision
 			count = 0;
@@ -107,7 +123,7 @@ public class Metrics{
 				}
 			}
 			precision += (double) count / (top10Components.size());
-			System.out.println("precision@"+top10Components.size()+": " + (double) count / top10Components.size());
+//			System.out.println("precision@"+top10Components.size()+": " + (double) count / top10Components.size());
 			
 			// HitRate
 			for(Map.Entry<Component, Double> recommendedComp : top3Components.entrySet()) {
@@ -123,12 +139,28 @@ public class Metrics{
 		meanPrecision = (double) precision/sizeOfTestingSet;
 		f1Score = (double) 2 / ((1/meanPrecision) + (1/meanRecall));
 		
+		System.out.println();
+		System.out.println("damping factor: " + dampingFactor + "  sweepRatio: "+ sweepRatio + "  normalization: " + normalization);
 		System.out.println("AUC mean: "+ sumOfAUC/N);
 		System.out.println("mean recall: " + meanRecall);
 		System.out.println("mean pricision: " + meanPrecision);
 		System.out.println("f1 score: " + f1Score);
 		System.out.println("Hit Rate: " + (float) hits/sizeOfTestingSet);
 		System.out.println("Coverage: " + coverage.getCoverage());
+		
+		String text = "damping factor: " + dampingFactor + "  sweepRatio: "+ sweepRatio + "  normalization: " + normalization + "\n" + 
+				"AUC mean: "+ sumOfAUC/N + "\n" + 
+				"mean recall: " + meanRecall + "\n" + 
+				"mean pricision: " + meanPrecision + "\n" + 
+				"f1 score: " + f1Score + "\n" + 
+				"Hit Rate: " + (float) hits/sizeOfTestingSet + "\n" + 
+				"Coverage: " + coverage.getCoverage() + "\n" + "\n"; 
+		try {
+			fileWriter.append(text);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+				
 	}
 
 }
